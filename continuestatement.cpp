96b9daefdae7c4bd6a0cedb8b040d83b03e98f92
parvesh <choudharyparvesht@gmail.com>
2026-06-14 18:29:20 +0530

    hideing a number 't' using break statement

---
+++ continuestatement.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main (){
+    int x, t;
+
+     cout << "choose the number to hide between -3 to 12"<<endl;
+     cin >> t;
+    for(x=12; x>-4; x--){
+
+    if(x==t)
+
+    continue;    
+
+   
+    
+    cout <<x <<endl;
+
+
+}
+    
+return 0;
+
+}

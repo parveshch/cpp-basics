ad69884242a3e83deecf435559a16c6809625879
parvesh <choudharyparvesht@gmail.com>
2026-06-21 20:52:57 +0530

    very basic program using structs

--- structuresbasics.cpp
+++ structuresbasics.cpp
@@ -4,24 +4,35 @@
 using namespace std;
 
 
-struct student
+typedef struct student
 {
     int rollno;
     int marks;
     char favChar;
 
     
-};
+} st;
 
 int main (){
-    struct student priyanshu;
+    st priyanshu;
     priyanshu.rollno = 21;
     priyanshu.marks = 87;
     priyanshu.favChar = 'w';
 
+    st ajit;
+    ajit.marks = 65;
+    ajit.rollno = 01;
+    ajit.favChar = 'a';
+
     cout << "The roll no is: "<< priyanshu.rollno<<endl;
     cout << "The marks are: "<< priyanshu.marks<<endl;
-    cout << "The fav char is: "<< priyanshu.favChar<<endl;
+    cout << "The fav char is: "<< priyanshu.favChar<<endl<<endl;
+
+    cout << "The roll no is: "<< ajit.rollno<<endl;
+    cout << "The marks are: "<< ajit.marks<<endl;
+    cout << "The fav char is: "<< ajit.favChar<<endl;
+
+
 
 return 0;
 }
